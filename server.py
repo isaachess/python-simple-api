494a268b970076a3c467d2ada9eaf6d2e559f630
Isaac Hess <isaachess@gmail.com>
2015-09-02 08:21:51 -0600

    Change everything over to snake case

--- server.py
+++ server.py
@@ -6,45 +6,46 @@ import random
 ## Helpers ###
 ##############
 
-def valueInEnum(value, allowedValues):
-    return _.contains(allowedValues, value)
+def value_in_enum(value, allowed_values):
+    return _.contains(allowed_values, value)
 
-def valueInRange(value, allowedRange):
-    return value >= allowedRange['min'] and value <= allowedRange['max']
+def value_in_range(value, allowed_range):
+    return value >= allowed_range['min'] and value <= allowed_range['max']
 
 #######################
 ### Response helpers ##
 #######################
 
-def genericFailure(message='Failure', statusCode=404):
+def generic_failure(message='Failure', status_code=404):
     return {
         'message': message,
-        'statusCode': statusCode
+        'status_code': status_code
     }
 
-def genericSuccess(payload, message='Success'):
+def generic_success(payload, message='Success', status_code=200):
     return {
         'payload': payload,
-        'message': message
+        'message': message,
+        'status_code': status_code
     }
 
-def itemNotFound():
-    return genericFailure('The requested item is not available.')
+def item_not_found():
+    return generic_failure('The requested item is not available.')
 
-def sendIfFound(item, successMessage='Success'):
+def send_if_found(item, success_message='Success'):
     if item is None:
-        return itemNotFound()
+        return item_not_found()
     else:
-        return genericSuccess(item, successMessage)
+        return generic_success(item, success_message)
 
 ###########
 ## CRUD ###
 ###########
 
-operatingModes = ['cool', 'heat', 'off']
-fanModes = ['off', 'auto']
-coolSetpointRange = {'min': 30, 'max': 100}
-heatSetpointRange = {'min': 30, 'max': 100}
+operating_modes = ['cool', 'heat', 'off']
+fan_modes = ['off', 'auto']
+cool_setpoint_range = {'min': 30, 'max': 100}
+heat_setpoint_range = {'min': 30, 'max': 100}
 
 thermostats = {
     'thermo1': {
@@ -73,32 +74,32 @@ def get(id):
 def update(id, updates):
     if id in thermostats:
         thermostats[id] = _.merge(thermostats[id], updates)
-        return genericSuccess(get(id))
+        return generic_success(get(id))
     else:
-        return genericFailure('Cannot update unknown resource.')
+        return generic_failure('Cannot update unknown resource.')
 
 ##################
 ## Model layer ###
 ##################
 
-def getThermo(id):
-    return sendIfFound(get(id))
+def get_thermo(id):
+    return send_if_found(get(id))
 
-def getProperty(id, prop):
+def get_property(id, prop):
     thermo = get(id)
     if thermo is None:
-        return itemNotFound()
+        return item_not_found()
     value = thermo.get(prop, None)
-    return sendIfFound(value)
+    return send_if_found(value)
 
-def getName(id):
-    return getProperty(id, 'name')
+def get_name(id):
+    return get_property(id, 'name')
 
-def setName(id, newName):
-    return update(id, {'name': newName})
+def set_name(id, new_name):
+    return update(id, {'name': new_name})
 
-def getTemp(id):
-    return getProperty(id, 'current_temp')
+def get_temp(id):
+    return get_property(id, 'current_temp')
 
 ##############
 ## Routing ###
@@ -116,19 +117,27 @@ urls = (
 
 class thermostat:
     def GET(self, id):
-        return getThermo(id)
+        return get_thermo(id)
 
 class name:
     def GET(self, id):
-        return getName(id)
+        return get_name(id)
 
     def PUT(self, id):
-        newName = web.data()
-        return setName(id, newName)
+        new_name = web.data()
+        return set_name(id, new_name)
 
 class temp:
     def GET(self, id):
-        return getTemp(id)
+        return get_temp(id)
+
+class operating_mode:
+    def GET(self, id):
+        return getOperatingMode(id)
+
+    def PUT(self, id):
+        new_name = web.data()
+        return set_name(id, new_name)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
